19759896378bd81397aa96a384a427daa6817d94
Cauã Augusto <cauaaugusto2.00@gmail.com>
2024-11-14 13:07:52 -0300

    atualização

--- Login/script.js
+++ Login/script.js
@@ -1,7 +1,7 @@
 const form = document.getElementById('form');
 const campos = document.querySelectorAll('.required');
 const spans = document.querySelectorAll('.span-required');
-const emailRegex = /^[a-z0-9.]+@[a-z0-9]+\.[a-z]+\.[a-z]?$/i;
+const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+.[a-zA-Z]{2,}$/;
 
 function setError(index){
     campos[index].style.border = '2px solid #e63636';
@@ -10,15 +10,54 @@ function setError(index){
 
 function erroRemove(index){
     campos[index].style.border = '';
-    spans[index],style.display = 'none';
+    spans[index].style.display = 'none';
 }
 
 function nameValidate(){
-    if(campos[0].value.length <4){
-        console.log('NOME DEVE TER 4 CARACTERES');
+    if(campos[0].value.length < 4)
+    {
+        setError(0);
+    }
+
+    else
+    {
+        erroRemove(0);
+    }
+
+}
+
+function emailValidate(){
+    if (emailRegex.test(campos[1].value)) 
+    {
+        setError(1);
+    }
+
+    else
+    {
+        erroRemove(1);
+    }
+}
+
+function passwordValidate(){
+    if(campos[2].value.length < 8)
+    {
+        setError(2);
     }
 
     else{
-        console.log('NOME VALIDADO')
+        erroRemove(2);
+        confirmPassword();
+    }
+}
+
+function confirmPassword(){
+    if(campos[2].value == campos[3].value )
+    {
+        erroRemove(3);
+    }
+
+    else
+    {
+        setError(3);
     }
 }
